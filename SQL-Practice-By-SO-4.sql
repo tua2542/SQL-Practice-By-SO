bdf279711bedb539660d0504fbe118b6d3abf022
tua2542 <42030288+tua2542@users.noreply.github.com>
2022-11-09 13:37:12 +0700

    [COMPLETED] SQL practice 4 from 13 to 25

--- SQL-Practice-By-SO-4.sql
+++ SQL-Practice-By-SO-4.sql
@@ -58,27 +58,70 @@ group by customer_id, ord_date
 having max(purch_amt) > 2000
 
 -- 13
+select customer_id, ord_date, max(purch_amt) as max
+from orders
+group by customer_id, ord_date
+having max(purch_amt) between 2000 and 6000
 
 -- 14
+select customer_id, ord_date, max(purch_amt) as max
+from orders
+group by customer_id, ord_date
+having max(purch_amt) in (2000, 3000, 5760, 6000)
 
 -- 15
+select customer_id, max(purch_amt) as max
+from orders
+group by customer_id
+having customer_id between 3002 and 3007
+order by max(purch_amt) asc
 
 -- 16
+select customer_id, max(purch_amt) as max
+from orders
+group by customer_id
+having customer_id between 3002 and 3007
+and max(purch_amt) > 1000
 
 -- 17
+select salesman_id, max(purch_amt) as max
+from orders
+group by salesman_id
+having salesman_id between 5003 and 5008
 
 -- 18
+select count(*) as count
+from orders
+where ord_date = '2012-08-17'
 
 -- 19
+select count(salesman_id) as count
+from salesman
 
 -- 20
+select ord_date, salesman_id, count(*) as count
+from orders
+group by ord_date, salesman_id
 
 -- 21
+select avg(PRO_PRICE) as "Average Price"
+from item_mast
 
 -- 22
+select count(*) as "Number of Products"
+from item_mast
+where PRO_PRICE >= 350
 
 -- 23
+select avg(PRO_PRICE) as "Average Price", PRO_COM as "Company ID"
+from item_mast
+group by PRO_COM
 
 -- 24
+select sum(DPT_ALLOTMENT) as sum 
+from emp_department
 
 -- 25
+select EMP_DEPT, count(*) as count
+from emp_details
+group by EMP_DEPT
