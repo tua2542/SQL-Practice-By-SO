caf54f744ff51689d107ff06de1a9b98298af1a6
tua2542 <42030288+tua2542@users.noreply.github.com>
2022-11-09 13:50:04 +0700

    [IN PROGRESS] SQL practice 5 from 1 to NA

---
+++ SQL-Practice-By-SO-5.sql
@@ -0,0 +1,25 @@
+-- Formatting Output - Exercises, Practice, Solution
+-- Reference: https://www.w3resource.com/sql-exercises/sql-fromatting-output-exercises.php
+
+-- 1
+select salesman_id, name, city , 
+       commission * 100 as "percent of commission"
+from salesman
+
+-- 2
+
+-- 3
+
+-- 4
+
+-- 5
+
+-- 6
+
+-- 7
+
+-- 8
+
+-- 9
+
+-- 10
