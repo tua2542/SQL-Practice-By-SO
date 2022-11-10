991b60fb5c4506bc5c75f7f43a8ba14f09822932
tua2542 <42030288+tua2542@users.noreply.github.com>
2022-11-10 11:25:13 +0700

    [COMPLETED] SQL practice 5 from 2 to 10

--- SQL-Practice-By-SO-5.sql
+++ SQL-Practice-By-SO-5.sql
@@ -7,19 +7,50 @@ select salesman_id, name, city ,
 from salesman
 
 -- 2
+select 'For', ord_date, count(*), ',there are', 'orders.'
+from orders
+group by ord_date
 
 -- 3
+select *
+from orders
+order by ord_no asc
 
 -- 4
+select *
+from orders
+order by ord_no desc
 
 -- 5
+select *
+from orders
+order by ord_date, purch_amt desc
 
 -- 6
+select cust_name, city, grade
+from customer
+order by customer_id
 
 -- 7
+select salesman_id, ord_date, max(purch_amt)
+from orders
+group by salesman_id, ord_date
+order by salesman_id, ord_date asc
 
 -- 8
+select cust_name, city, grade
+from customer 
+order by 3 desc
 
 -- 9
+select customer_id, count(distinct ord_no), max(purch_amt)
+from orders
+group by customer_id
+order by 2 desc
 
 -- 10
+select ord_date, sum(purch_amt) as "purchase amount" , 
+       sum(purch_amt) * 15/100 as "total commission"
+from orders
+group by ord_date
+order by ord_date
